19ef320114bef3a337d9265d1a5dad7f162d0620
Zihao <zihaozhang1@gmail.com>
2020-11-02 00:03:27 -0500

    lowercase prompt added, password generates, storage added

--- Develop/script.js
+++ Develop/script.js
@@ -13,17 +13,34 @@ function writePassword() {
   var password = "";
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordSet = "";
 
 // Password prompt
-  var passwordLengthInput = prompt("How many characters would you like your password to be?")
+  var passwordLengthInput = prompt("How many characters would you like your password to contain?");
   console.log(passwordLengthInput)
   if (Number(passwordLengthInput < 8) | Number(passwordLengthInput > 128) | isNaN(passwordLengthInput))
   {
   alert("Please enter a number between 8 and 128");
   return;
   }
-}
+
+// Lowercase prompt
+  var lowercaseInput = confirm("Would you like lower letters to be included in your password");
+  if (lowercaseInput) {
+    passwordSet += lowercase;
+  }
+
+//Password display
+  if (Number(passwordLengthInput >= 8) && Number(passwordLengthInput <= 128)) {
+    for (var i = 0; i < passwordLengthInput; i++) {
+        password += passwordSet.charAt(Math.floor(Math.random() * passwordSet.length));
+    }; 
+// Storage
+  localStorage.setItem("password", password);
+  var passwordStore = localStorage.getItem("password");
+  passwordText.textContent = passwordStore;
+  };
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
