a217d301c1982792e4909943f0251ac2d0fd317a
Zihao <zihaozhang1@gmail.com>
2020-11-01 23:02:32 -0500

    password prompt

--- Develop/script.js
+++ Develop/script.js
@@ -3,18 +3,26 @@
 var lowercase = "qwertyuiopasdfghjklzxcvbnm";
 var uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var numbers   = "0123456789";
-var special   = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var special   = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = "";
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+// Password prompt
+  var passwordLengthInput = prompt("How many characters would you like your password to be?")
+  console.log(passwordLengthInput)
+  if (Number(passwordLengthInput < 8) | Number(passwordLengthInput > 128) | isNaN(passwordLengthInput))
+  {
+  alert("Please enter a number between 8 and 128");
+  return;
+  }
 }
 
 // Add event listener to generate button
